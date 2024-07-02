1119c09c643fda37781765abbf4c075d87e8b6c1
Krishnadhar Pandey <krishnadhargzp00@gmail.com>
2024-07-02 18:33:01 +0530

    first commit

---
+++ 01_absolute.cpp
@@ -0,0 +1,16 @@
+// wap to convert to the absolute value
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    if(x>=0)
+    {
+        cout<<x<<endl;
+    }
+    else
+    {
+        cout<<-x<<endl;
+    }
+}
